9daa11e987dad43677df4c3ae6dddb4e2f929396
shamma077 <102911773+shamma077@users.noreply.github.com>
2023-09-08 23:29:55 +0530

    corrrection in form page

--- 10YSolved/src/app/add-question/add-question.component.ts
+++ 10YSolved/src/app/add-question/add-question.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-add-question',
   templateUrl: './add-question.component.html',
   styleUrls: ['./add-question.component.scss']
 })
-export class AddQuestionComponent {
+export class AddQuestionComponent  implements OnInit {
 
+  QuestionEnglish:string= "";
+  QuestionHindi:string= "";
+  Option:object={};
+  CorrectAnswer:string= "";
+  Solutions:string= "";
+  CreatedDate: string= "";
+  Year: string="";
+  ExamType:string="";
+  ExamSubType:string="";
+  constructor() { }
+
+  ngOnInit() {
+  }
+    // myFunc()
+  //  console.log(this.QuestionEnglish);
+  //   console.log(this.company);
+  //   console.log(this.email);
+  //   console.log(this.lastname);
+  //   console.log(this.title);
+  //   console.log(this.phoneNum);
+  //   console.log(this.needHelp);
+   
+  // }
 }
